954993837168c0fc3863d8652c7dbfb0c14df5bf
leonardo.pereira1 <leonardo.pereira@acct.global>
2020-01-19 01:38:01 -0300

    Flutter GIT search directory update

---
+++ buscador_de_gif/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_de_gif/ui/home_page.dart';
+
+void main() => runApp(MaterialApp(
+    home: HomePage(),
+    title: "Buscador de GIF",
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(hintColor: Colors.white)));
