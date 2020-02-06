f3210fc56ea327abdc0266b376cc6f3e007479a3
leonardo.pereira1 <leonardo.pereira@acct.global>
2020-02-05 22:03:42 -0300

    Updating files

--- alone_buscador_de_gifs/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:alone_buscador_de_gifs/ui/home.dart';
-
-void main() => runApp(MaterialApp(
-      home: Home(),
-      debugShowCheckedModeBanner: false,
-    ));
